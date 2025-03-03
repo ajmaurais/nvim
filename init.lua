30901c49c57fa2d5aa3fa6298fb8274b2580e7e1
mauraisa <mauraisa@uw.edu>
2025-03-03 11:47:43 -0800

    Add global settings.

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ require("config.lazy")
 
 vim.cmd([[
     colorscheme monokai-pro
+    set background=dark
+
+    " Use the system register for everything
+    set clipboard=unnamedplus
+
     set mouse=
     set expandtab           " enter spaces when tab is pressed
     set tabstop=4           " use 4 spaces to represent tab
@@ -10,7 +15,9 @@ vim.cmd([[
     set autoindent          " copy indent from current line when starting a new line
     set smartindent
     set smarttab
-    
+
+    " change leader timeout and mapping
+    set notimeout nottimeout
 
     set ruler                           " show line and column number
     set showcmd             " show (partial) command in status line
@@ -26,4 +33,10 @@ vim.cmd([[
     nnoremap <CR> :noh<CR>
 
     set autoread "auto load file changes
+    set autowriteall "auto write file modifications
+
+    if has('persistent_undo')      "check if your vim version supports it
+      set undofile                 "turn on the feature
+      set undodir=$HOME/.vim/undo  "directory where the undo files will be stored
+    endif
 ]])
