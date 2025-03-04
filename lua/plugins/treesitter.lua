0818e876a0dd1e01261eb0538558938c59b5c4f7
Aaron Maurais <mauraisa@uw.edu>
2025-03-03 16:58:05 -0800

    macos specific changes to treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,14 +4,11 @@ return {
         build = ":TSUpdate",
         config = function ()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "markdown", "markdown_inline", "latex", "r", "rnoweb", "yaml", "csv" },
+                ensure_installed = { "lua", "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" },
                 highlight = { enable = true },
                 sync_install = false,      -- important
                 auto_install = false,      -- important
             })
         end
     },
-    { "nvim-treesitter/nvim-treesitter-textobjects" },
-    { "nvim-treesitter/nvim-treesitter-context" },
-    -- { "nvim-treesitter/nvim-markdown" },
 }
