c06b1d8f300984c331087e4a73cdbf384c183904
Aaron Maurais <mauraisa@uw.edu>
2025-03-06 13:22:44 -0800

    Use better vim-tmux-navigator plugin.

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,19 +1,18 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  lazy = false,
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-w>h", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<c-w>j", "<cmd>TmuxNavigateDown<cr>" },
-    { "<c-w>k", "<cmd>TmuxNavigateUp<cr>" },
-    { "<c-w>l", "<cmd>TmuxNavigateRight<cr>" },
-    { "<c-w>\\", "<cmd>TmuxNavigatePrevious<cr>" },
-  },
+    'alexghergh/nvim-tmux-navigation',
+    lazy = false,
+    config = function()
+
+        local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+        nvim_tmux_nav.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+
+        vim.keymap.set('n', "<C-w>h", nvim_tmux_nav.NvimTmuxNavigateLeft)
+        vim.keymap.set('n', "<C-w>j", nvim_tmux_nav.NvimTmuxNavigateDown)
+        vim.keymap.set('n', "<C-w>k", nvim_tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', "<C-w>l", nvim_tmux_nav.NvimTmuxNavigateRight)
+        vim.keymap.set('n', "<C-w>\\", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    end,
 }
