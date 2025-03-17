c3eb6709565d20ca713425954abced51ba48a4fa
Aaron Maurais <mauraisa@uw.edu>
2025-03-17 11:29:31 -0700

    Fix R settings.

--- lua/plugins/R.lua
+++ lua/plugins/R.lua
@@ -14,6 +14,7 @@ return {
                 end
             },
             R_args = {"--quiet", "--no-save"},
+            R_nvim_wd = 1,
             min_editor_width = 90,
             rconsole_width = 110,
             source_args = "echo = TRUE",
@@ -22,11 +23,15 @@ return {
                 "RSPlot",
                 "RSaveClose",
             },
-         view_df = {
-              n_lines = 0,
-              -- save_fun = "function(obj, obj_name) {temp_file <- paste0('/tmp/', obj_name, '.csv'); write.table(obj, file=temp_file, row.name=F, sep=',')}",
-   open_fun = "ssview <- function(data){ temp_file <- paste0(tempfile(), '.csv'); write.table(data, file=temp_file, row.name=F, sep=','); system(paste0('wslview $(wslpath -w ', temp_file, ')')) }; ssview(%s)"
-         }
+            quarto_chunk_hl = {
+                highlight = false,               -- Disable the highlighting
+                virtual_title = true,           -- Don't add the virtual text
+                -- bg = "#003010",                  -- Use a different background color
+                events = "BufEnter,TextChanged", -- Update the highlighting more often
+            },
+            -- view_df = {
+            --     n_lines = 0
+            -- }
         }
         require("r").setup(opts)
     end,
