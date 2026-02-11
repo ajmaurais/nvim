1cdf0c75c9e2d36deb54131a8ff770f6bbada47b
aaron-maurais <aaron-maurais@idexx.com>
2026-02-11 14:15:13 -0500

    Hopefully fix linux R.nvim errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,14 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function ()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" },
-                highlight = { enable = true },
-                sync_install = false,      -- important
-                auto_install = false,      -- important
-            })
+            require("nvim-treesitter").setup()
+            local langs = { "lua", "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" }
+            local installed = require("nvim-treesitter").get_installed()
+            for _, lang in ipairs(langs) do
+                if not vim.list_contains(installed, lang) then
+                    require("nvim-treesitter").install({ lang })
+                end
+            end
         end
     },
 }
